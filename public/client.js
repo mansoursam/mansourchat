3d7cd14d7fbb9e922b765642e64862fb5e6e29ee
mansoursam <sam.mansour@protonmail.com>
2018-11-29 15:53:03 +0100

    My first commit

---
+++ public/client.js
@@ -0,0 +1,76 @@
+var socket = io.connect('http://localhost:8800')
+//Query DOM
+var message = document.getElementById('message');
+formElement = document.getElementById('formElement')
+username = document.getElementById('username');
+output = document.getElementById('output');
+button = document.getElementById('send');
+feedback = document.getElementById('feedback')
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+var d = new Date();
+var m = addZero(d.getMinutes());
+var h = addZero(d.getHours());
+var messageTime = h + ":" + m
+console.log(h + ":" + m)
+// Emit events
+formElement.addEventListener('submit', (e) => {
+    e.preventDefault()
+    socket.emit('chat', {
+        username: username.value,
+        message: message.value,
+        time: messageTime
+    })
+    formElement.reset()
+});
+message.addEventListener('keypress', () => {
+    socket.emit('typing', username.value);
+});
+socket.on('typing', (data) => {
+    feedback.innerHTML = data + ' is typing ...';
+    console.log(data);
+})
+let messagesArray = []
+let renderMessages = (array) => {
+    console.log(array);
+    let html = ''
+
+    array.forEach(data => {
+        console.log(data);
+        html += '<div><p><strong>' + data.username + ':</strong>' + " " + data.message + '</p>' + '<sub style="color:grey"><i>' + data.time + '</i></sub></div>';
+
+    });
+    output.innerHTML = html
+
+}
+socket.on('Chat', (message) => {
+    console.log(message)
+    messagesArray.push(message)
+    renderMessages(messagesArray)
+    console.log(messagesArray);
+})
+
+const userFormElement = document.getElementById('usernameElement')
+const userInputElement = document.getElementById('username')
+let loginName = ''
+userFormElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+    loginName = userInputElement.value
+    userFormElement.classList.toggle('hidden')
+    document.getElementById('logInmessage').innerHTML = 'you are logged as ' + '<h5><b><i>' + loginName + '</i></b></h5>';
+    socket.emit('login', loginName)
+})
+socket.on('login', (messages) => {
+    messagesArray = [...messages]
+    renderMessages(messagesArray)
+})
+socket.on('userCount', (data) => {
+    connectedusers.innerHTML = '<h6><b>' + data.user + ' users are connected</b></h6>'
+})
+
+
+
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const fs = require('fs')
+const app = express();
+const moment = require('moment');
+
+app.use(express.static('public'));
+var server = app.listen(8800, () => {
+    console.log('Server started');
+});
+
+const io = socketIO(server)
+var connections = []
+var connectedUsers = 0
+io.on('connection', (socket) => {
+    connections.push(socket)
+    connectedUsers++;
+    io.sockets.emit('userCount', {
+        user: connectedUsers
+    })
+    console.log(`${connections.length}user is connected`);
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1)
+        connectedUsers--;
+        io.sockets.emit('userCount', {
+            user: connectedUsers
+        })
+        console.log(`${connections.length} user  connected `)
+    })
+    socket.on('chat', (message) => {
+        fs.readFile('./public/chat.json', (err, data) => {
+            if (err) throw err
+            let json = JSON.parse(data)
+            console.log(data);
+            json.push(message)
+            console.log(message)
+            fs.writeFile('./public/chat.json', JSON.stringify(json), (err) => {
+                console.log(json)
+            })
+        })
+        io.emit('Chat', message)
+    })
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
+    socket.on('login', (username) => {
+        fs.readFile('./public/chat.json', (err, data) => {
+            if (err) console.log(err)
+            const json = JSON.parse(data)
+            socket.emit('login', json)
+        })
+    })
+})
